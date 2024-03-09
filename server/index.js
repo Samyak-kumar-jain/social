c3402acc8a325957578808929661cb53896991af
samyak jain <dosomework2@gmail.com>
2024-03-09 15:00:12 +0530

    h

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import dotenv from "dotenv"
+import dotenv from 'dotenv';
 import router from './routes/authroute.js';
 import cors from 'cors';
-// import  from './routes/post.route.js';
-
 
 const app = express();
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
-app.use(cors({
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(
+  cors({
     origin: 'https://social-8bnm.vercel.app', // Adjust this to your frontend origin
-  }));;
+  })
+);
 
 dotenv.config();
 
 try {
-    await mongoose.connect(process.env.MONGODB_URI || 5000, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("MongoDB connected");
-    app.listen(process.env.PORT || 5000, () => console.log("App listening on port 5000"));
+  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('MongoDB connected');
+  app.listen(process.env.PORT || 5000, () => console.log('App listening on port 5000'));
 } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+  console.error('Error connecting to MongoDB:', error);
 }
 
-    app.use('/auth', router)
-    // app.use('/post', router)
-
-
+app.use('/auth', router);
