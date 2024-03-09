f68c0fa6bee0234b72008a29c1e8ad2dc15b066c
samyak jain <dosomework2@gmail.com>
2024-03-09 14:36:15 +0530

    hey

--- Social-platform/src/components/Authentication.jsx
+++ Social-platform/src/components/Authentication.jsx
@@ -46,7 +46,7 @@ function Authentication({onLoginSuccess}) {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': 'https://social-8bnm.vercel.app'
+                
               },
               body: JSON.stringify(data),
             });
