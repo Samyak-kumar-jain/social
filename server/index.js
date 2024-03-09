581e69c00ff9e58e10f3cf794b3df27080e84a54
samyak jain <dosomework2@gmail.com>
2024-03-09 15:02:28 +0530

    samyak

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(
   cors({
-    origin: 'https://social-8bnm.vercel.app', // Adjust this to your frontend origin
+    origin: 'https://social-8bnm.vercel.app', // Adjust this to your frtend origin
   })
 );
 
